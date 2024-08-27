041c6f97bdc2c4dfd9d935240707c9f274741cf7
marktipton <marktipton95@gmail.com>
2024-08-27 13:41:26 -0500

    social links working correctly

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -5,9 +5,12 @@ import  github from '../assets/github.svg';
 export function SocialLinks() {
   return (
     <div className="social-links">
-      <img src={linkedIn} alt="" />
-
-      <img src={github} alt="" />
+      <a href="https://www.linkedin.com/in/marktipton1/" target="_blank" rel="noopener noreferrer">
+        <img src={linkedIn} alt="linkedin" />
+      </a>
+      <a href="https://github.com/marktipton/atlas-react-and-jsx" target="_blank" rel="noopener noreferrer">
+        <img src={github} alt="github" />
+      </a>
     </div>
   );
 }
