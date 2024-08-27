298e9a6521522318511c6321de3d8d421989fe45
marktipton <marktipton95@gmail.com>
2024-08-27 13:02:05 -0500

    added greetings and svgs depending on time

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
 
-export function Greeting(props) {
+export function Greeting() {
+  const time = new Date().getHours();
+  let imgSource = '';
+  let greetingText = '';
+
+  if (time < 6 || time > 21) {
+    imgSource = '../assets/night.svg';
+    greetingText = 'Good Night!';
+  } else if (time < 12) {
+    imgSource = '../assets/day.svg';
+    greetingText = 'Good Morning!';
+  } else if (time < 17) {
+    imgSource = '../assets/day.svg';
+    greetingText = 'Good Afternoon!';
+  } else {
+    imgSource = '../assets/evening.svg';
+    greetingText = 'Good Evening!';
+  }
+
   return (
     <h1 className='greeting'>
-      <img src="" alt="" />
+      <img src={imgSource} alt="GreetingImg" />
+      {greetingText}
     </h1>
   );
 }
